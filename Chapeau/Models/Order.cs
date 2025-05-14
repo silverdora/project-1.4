1c719c6758b949a6a517131d9d7a62f421c9b3cb
MatheusFracasso <725283@student.inholland.nl>
2025-05-14 11:58:40 +0200

    Models: Order, Includes, and MenuItem- First version

---
+++ Chapeau/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace Chapeau.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int EmployeeID { get; set; }
+        public int TableID { get; set; }
+        public DateTime OrderTime { get; set; }
+        public bool IsServed { get; set; }
+
+        public List<Includes> Includes { get; set; } = new List<Includes>();
+    }
+}
