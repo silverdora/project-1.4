87afea80c7ef35dc4cff7b98b18ea223e9ff3529
MatheusFracasso <725283@student.inholland.nl>
2025-05-15 20:11:01 +0200

    Enum settle corectly

---
+++ Chapeau/Models/Enums.cs
@@ -0,0 +1,26 @@
+﻿namespace Chapeau.Models
+{
+    public class Enums
+    {
+        public enum MenuCard
+        {
+            Lunch,
+            Diner,
+            Drinks
+        }
+
+        public enum MenuCategory
+        {
+            Starters,
+            Mains,
+            Desserts,
+            Entremets,
+            Beer,
+            Wine,
+            Spirits,
+            CoffeeTea,
+            SoftDrinks
+        }
+
+    }
+}
