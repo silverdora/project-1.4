6cdfabf6e30628733ca305384822bf048ea40c10
728287Luwana <728287@student.inholland.nl>
2025-05-18 22:47:09 +0200

    PaymentServices

--- Chapeau/Services/PaymentServices.cs
+++ Chapeau/Services/PaymentServices.cs
@@ -1,6 +1,22 @@
-﻿namespace Chapeau.Services
+﻿using Chapeau.Models;
+using Chapeau.Repositories.Interfaces;
+using Chapeau.Services.Interface;
+
+namespace Chapeau.Services
 {
-    public class PaymentServices
+    public class PaymentService : IPaymentService
     {
+        private readonly IPaymentRepository _paymentRepository;
+
+        public PaymentService(IPaymentRepository paymentRepository)
+        {
+            _paymentRepository = paymentRepository;
+        }
+
+        public List<Payment> GetAllPayments(int orderId)
+        {
+            return _paymentRepository.GetAllPayments();
+        }
+
     }
 }
