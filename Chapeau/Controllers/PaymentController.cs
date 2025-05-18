6e642d6592a4508ea1e6417691ecdfe36e06a608
728287Luwana <728287@student.inholland.nl>
2025-05-18 22:38:51 +0200

    using

---
+++ Chapeau/Controllers/PaymentController.cs
@@ -0,0 +1,18 @@
+﻿using Chapeau.Repositories.Interfaces;
+using Chapeau.Repositories;
+using Chapeau.Services;
+using Chapeau.Services.Interfaces;
+using Chapeau.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapeau.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
