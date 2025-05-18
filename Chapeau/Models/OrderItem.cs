8ff00e55418365a2ad8745de80c921144a4a6d70
Daria Gakhova <727546@student.inholland.nl>
2025-05-18 22:31:50 +0200

    final

--- Chapeau/Models/OrderItem.cs
+++ Chapeau/Models/OrderItem.cs
@@ -6,6 +6,14 @@
         public MenuItem MenuItem { get; set; }
         public int ItemID { get; set; }
         public DateTime IncludeDate { get; set; }
+        public TimeSpan WaitingTime
+        {
+            get
+            {
+                return DateTime.Now - IncludeDate;
+            }
+        }
+
         public Status Status { get; set; }
         public int Quantity { get; set; }
 
