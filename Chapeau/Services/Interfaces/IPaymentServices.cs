1b3b0e745ee29a46f4237403e5f14791fd5ab2fc
728287Luwana <728287@student.inholland.nl>
2025-05-20 15:31:18 +0200

    adding processPayment

--- Chapeau/Services/Interfaces/IPaymentServices.cs
+++ Chapeau/Services/Interfaces/IPaymentServices.cs
@@ -5,6 +5,8 @@ namespace Chapeau.Services.Interfaces
     public interface IPaymentService
     {
         List<Payment> GetAllPayments(int orderId);
+        void ProcessPaymentForOrder(Order order, string paymentType, decimal tip);
+
         // ...
     }
 }
