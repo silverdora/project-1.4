66857722a046f52300be1c49b695c573850ade74
728287Luwana <728287@student.inholland.nl>
2025-06-02 18:18:21 +0200

    Creating FinishOrderViewModel

---
+++ Chapeau/ViewModels/FinishOrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Chapeau.ViewModels
+{
+    public class FinishOrderViewModel
+    {
+        public int OrderID { get; set; }
+        public decimal AmountPaid { get; set; }
+        public decimal TipAmount { get; set; }
+        public string PaymentType { get; set; }
+        public string Feedback { get; set; } // Optional
+        public decimal LowVatAmount { get; set; }
+        public decimal HighVatAmount { get; set; }
+    }
+}
